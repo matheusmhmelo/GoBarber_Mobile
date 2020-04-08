5a55cd5cd8fcbfeb366ecd6078b1a3001eebc0f0
matheusmelo <ma_hrm@hotmail.com>
2020-04-07 23:41:08 -0300

    Application complete

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styed from 'styled-components/native';
+
+export default styed(LinearGradient).attrs({
+  colors: ['#7159c1', '#ab59c1'],
+})`
+  flex: 1;
+`;
